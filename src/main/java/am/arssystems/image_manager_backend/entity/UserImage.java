5e962796b02f1af0bc7a52d26f9da588680826b9
magam11 <magmurazyanas@gmail.com>
2019-07-08 11:18:30 +0400

    +++++

--- src/main/java/am/arssystems/image_manager_backend/entity/UserImage.java
+++ src/main/java/am/arssystems/image_manager_backend/entity/UserImage.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
@@ -28,9 +31,12 @@ public class UserImage {
     private double picSize;
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
-    @Column(name = "created_at",updatable = false)
+    @Column(name = "created_at",nullable = false,updatable = false)
     @JsonView(View.Base.class)
+//    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
     private Timestamp createdAt;
     @Column(name = "deleted_at")
     @JsonView(View.Base.class)
